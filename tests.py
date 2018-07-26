24b086a4e7ff1f9dd1b5a7585d72bfc6e63194d4
DustyShap <dustinshapiro@gmail.com>
2018-07-26 15:13:28 -0500

    testing

---
+++ tests.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from application import app
+
+class BasicTests(unittest.TestCase):
+    def test_main_page(self):
+        self.app = app.test_client()
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
