103fa73aa860747cff33118adce21877e98d46b3
DustyShap <dustinshapiro@gmail.com>
2018-07-26 13:26:26 -0500

    refactor functions

--- application.py
+++ application.py
@@ -18,6 +18,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 socketio = SocketIO(app)
 
+
 # Routes
 @app.route("/", methods=['POST', 'GET'])
 def index():
@@ -25,20 +26,25 @@ def index():
     is_admin = session.get('admin')
     all_users_list = []
     all_messages_list = []
-    all_users = get_all_users()
-    all_messages = get_all_messages()
+    all_users = User.get_all_users()
+    all_messages = Message.get_all_messages()
 
     for user in all_users:
         all_user_dict = {}
         all_user_dict['user'] = user
-        all_user_dict['text_count'] = user_total_texts(user.id)
+        all_user_dict['text_count'] = User.user_total_texts(user.id)
         all_users_list.append(all_user_dict)
-    return render_template('textline.html',all_users=all_users_list, all_messages=all_messages, is_admin=is_admin,total_texts=len(all_messages))
+    return render_template('textline.html',
+                           all_users=all_users_list,
+                           all_messages=all_messages,
+                           is_admin=is_admin,
+                           total_texts=len(all_messages))
 
 
 @app.route("/all_single_user_messages", methods=['POST'])
 def all_single_user_messages():
-    all_user_messages = get_all_user_messages(request.form['user_id_clicked'])
+    all_user_messages = Message.get_all_user_messages(
+                                            request.form['user_id_clicked'])
     all_user_messages_list = []
     for msg in all_user_messages:
         all_user_messages_dict = {}
@@ -50,24 +56,26 @@ def all_single_user_messages():
         all_user_messages_list.append(all_user_messages_dict)
     return jsonify(all_user_messages_list)
 
-@app.route("/search_for_user",methods=['POST'])
+
+@app.route("/search_for_user", methods=['POST'])
 def search_for_user():
     all_searched_users_list = []
     search_term = request.form['search_term']
-    users = User.query.filter(User.display_name.ilike("%"+search_term+"%") | User.phone_number.ilike("%"+search_term+"%")).all()
+    users = User.query.filter(
+                            User.display_name.ilike("%"+search_term+"%") |
+                            User.phone_number.ilike("%"+search_term+"%")).all()
     for user in users:
         all_searched_users_dict = {}
         all_searched_users_dict['user'] = user.display_name
         all_searched_users_dict['user_id'] = user.id
         all_searched_users_dict['number'] = user.phone_number
         all_searched_users_dict['location'] = user.location
-        all_searched_users_dict['text_count'] = user_total_texts(user.id)
+        all_searched_users_dict['text_count'] = User.user_total_texts(user.id)
         all_searched_users_list.append(all_searched_users_dict)
     return jsonify(all_searched_users_list)
 
-    return 'none'
 
-@app.route('/admin_login',methods=["GET","POST"])
+@app.route('/admin_login', methods=["GET", "POST"])
 def admin_login():
     error_message = ""
     if request.method == 'POST':
@@ -79,21 +87,22 @@ def admin_login():
             return redirect(url_for('index'))
         else:
             error_message = "Incorrect Password"
-            return render_template('admin_login.html',error_message=error_message)
+            return render_template('admin_login.html',
+                                   error_message=error_message)
     return render_template('admin_login.html')
 
 
 @app.route('/admin_logout')
 def admin_logout():
-    session.pop('admin',None)
+    session.pop('admin', None)
     return redirect(url_for('index'))
+
 
 @app.route('/sms', methods=['POST'])
 def sms():
-
     phone_number = request.form['From'][2:]
     message_body = request.form['Body']
-    from_city  = request.form['FromCity']
+    from_city = request.form['FromCity']
     from_state = request.form['FromState']
     timestamp = datetime.datetime.now().strftime("%-m/%d/%Y %-I:%M:%S%p")
     default_name = "The " + phone_number[:3]
@@ -103,27 +112,29 @@ def sms():
     user = User.query.filter_by(phone_number=phone_number).first()
 
     if user is None:
-        user = User(phone_number=phone_number,display_name=default_name,location=location)
+        user = User(phone_number=phone_number,
+                    display_name=default_name,
+                    location=location)
         db.session.add(user)
         db.session.commit()
         does_user_exist = False
     else:
         does_user_exist = True
 
-    message = Message(message_body=message_body,message_timestamp=timestamp,message_user_id=user.id)
+    message = Message(message_body=message_body,
+                      message_timestamp=timestamp,
+                      message_user_id=user.id)
     if num_media > 0:
         media = request.form["MediaUrl0"]
         message.message_media_url = media
     db.session.add(message)
     db.session.commit()
-    total_texts_from_user = user_total_texts(user.id)
-
 
     new_text = {
         "message": message.to_dict(),
         "user": user.to_dict(),
-        "does_user_exist":does_user_exist,
-        "total_texts_from_user":total_texts_from_user,
+        "does_user_exist": does_user_exist,
+        "total_texts_from_user": User.user_total_texts(user.id),
     }
 
     socketio.emit('msg received', new_text, broadcast=True)
@@ -131,35 +142,18 @@ def sms():
     resp.message("Your message has been sent to The Text Inbox")
     return str(resp)
 
+
 @app.route('/update_name', methods=['POST'])
 def update_name():
     new_name = request.form['new_name']
     number_to_update = request.form['number_to_update']
-    user_to_update = User.query.filter_by(phone_number=number_to_update).first()
+    user_to_update = User.query.filter_by(
+                                         phone_number=number_to_update).first()
     user_to_update.display_name = new_name
     db.session.commit()
     return redirect(url_for('index'))
 
 
-
 @app.before_request
 def make_session_permanent():
     session.permanent = True
-
-def get_all_users():
-    return db.session.query(User).join(Message, User.id==Message.message_user_id).order_by(desc(Message.id)).all()
-
-def get_all_specific_users(user_id):
-    return db.session.query(User).join(Message, User.id==Message.message_user_id).filter(User.id == user_id).order_by(desc(Message.id)).all()
-
-def get_all_messages():
-    return db.session.query(Message,User).join(User, Message.message_user_id == User.id).order_by(desc(Message.id)).all()
-
-def get_all_user_messages(user_id):
-    return db.session.query(Message,User).join(User,Message.message_user_id == User.id).filter(User.id == user_id).order_by(desc(Message.id)).all()
-
-def user_total_texts(user_id):
-    return Message.query.filter_by(message_user_id=user_id).count()
-
-def total_texts():
-    return len(get_all_messages())
